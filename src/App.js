8857329c9f7a4f7ff3191e7932fe21d5d0cda4fc
Farid <bouras.farid@outlook.fr>
2022-07-27 15:34:40 +0200

    feat: adeed component "header" V1 CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
+import Home from './pages/Home';
+import Offer from './pages/Offer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      'https://lereacteur-vinted-api.herokuapp.com/offers'
+    );
+    setData(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/home'>Home</Link>
+          </li>
+          <li>
+            <Link to='/offer'>Offer</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/Home' element={<Home />} />
+        <Route path='/Offer' element={<Offer />} />
+      </Routes>
+    </Router>
   );
 }
 
