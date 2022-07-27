44fa965a31360d7f6cbfdfbdec3ea2a546873648
Farid <bouras.farid@outlook.fr>
2022-07-27 15:33:54 +0200

    build: added react router (construction path)

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import logo_vinted from '../assets/logo_vinted.png';
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+      <div className='header-position'>
+        <img className='header-logo' src={logo_vinted} alt='logo_vinted' />
+        <div className='search-container'>
+          <div>
+            <i class='fa-solid fa-magnifying-glass'></i>
+            <input
+              type='text'
+              className='search-input'
+              placeholder='Recherche des articles'
+            />
+          </div>
+          <div className='container-sort'>
+            <div className='section-sort'>
+              <span>trier par prix :</span>
+              <span>...</span>
+              <span>Prix entre :</span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <button className='header-button'>S'inscrire</button>
+          <button className='header-button'>Se connecter</button>
+        </div>
+        <div>
+          <button className='button-sold'>Vends tes articles</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
