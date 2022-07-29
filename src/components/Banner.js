9176af1720904102c09d9c1f84e4e8dbaf5929dc
Farid <bouras.farid@outlook.fr>
2022-07-29 14:56:11 +0200

    style: correction "banner"

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+const Banner = () => {
+  return (
+    <div className='container-banner'>
+      <div className='banner-buy'>
+        <div>
+          <p>Prêt à faire du tri dans vos placards?</p>
+          <button>Commencer à vendre</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
