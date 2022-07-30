19f763f0880646ad12ae5735280aa7f2ca6cc331
Farid <bouras.farid@outlook.fr>
2022-07-30 17:25:05 +0200

    feat: added new direction in composant "Banner"

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
+
 const Banner = () => {
   return (
     <div className='container-banner'>
       <div className='banner-buy'>
         <div>
           <p>Prêt à faire du tri dans vos placards?</p>
-          <button>Commencer à vendre</button>
+
+          <Link to='/login'>
+            <button>Commerncer à vendre</button>
+          </Link>
         </div>
       </div>
     </div>
