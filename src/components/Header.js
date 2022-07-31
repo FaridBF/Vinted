4a735bf8baf9480c6ba7c8b8cccabbaa2fafd137
Farid <bouras.farid@outlook.fr>
2022-07-31 21:03:40 +0200

    feat: added useLocation (pathname login et signup) hidden container filter in the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import logo_vinted from '../assets/logo_vinted.png';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 import HeaderRange from './HeaderRange';
 
@@ -13,70 +13,132 @@ const Header = ({
 }) => {
   const navigate = useNavigate();
 
+  const location = useLocation();
+  const currentURLPathname = location.pathname;
+
   return (
     <>
-      <div className='header-container'>
-        <div className='header-position'>
-          <Link to='/'>
-            <img className='header-logo' src={logo_vinted} alt='logo_vinted' />
-          </Link>
-          <div className='search-container'>
-            <div>
-              <input
-                type='text'
-                className='search-input'
-                value={input}
-                placeholder='Recherche des articles'
-                onChange={(event) => {
-                  setInput(event.target.value);
-                }}
+      {currentURLPathname === '/' ? (
+        <div className='header-container'>
+          <div className='header-position'>
+            <Link to='/'>
+              <img
+                className='header-logo'
+                src={logo_vinted}
+                alt='logo_vinted'
               />
-            </div>
-            <div className='container-sort'>
-              <div className='section-sort'>
-                <span>trier par prix :</span>
-                <button
-                  className='toogleByPrice'
-                  onClick={() => setIsPriceAsc(!isPriceAsc)}
-                >
-                  {isPriceAsc ? 'close' : 'open'}
-                </button>
-                <span className='headerRange'>
-                  <HeaderRange />
-                </span>
+            </Link>
+            <div className='search-container'>
+              <div>
+                <input
+                  type='text'
+                  className='search-input'
+                  value={input}
+                  placeholder='Recherche des articles'
+                  onChange={(event) => {
+                    setInput(event.target.value);
+                  }}
+                />
+              </div>
+              <div className='container-sort'>
+                <div className='section-sort'>
+                  <span>trier par prix :</span>
+                  <button
+                    className='toogleByPrice'
+                    onClick={() => setIsPriceAsc(!isPriceAsc)}
+                  >
+                    {isPriceAsc ? 'close' : 'open'}
+                  </button>
+                  <span className='headerRange'>
+                    <HeaderRange />
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className='button-container'>
-            {token === null ? (
-              <>
-                <Link to='/signup'>
-                  <button className='header-button'>S'inscrire</button>
-                </Link>
-                <br />
-                <Link to='/login'>
-                  <button className='header-button'>Se connecter</button>
-                </Link>
-              </>
-            ) : (
-              <button
-                className='header-button'
-                onClick={() => {
-                  setUser(null);
-                  navigate('/');
-                }}
-              >
-                Se déconnecter
-              </button>
-            )}
-          </div>
-          <div>
-            <Link to='/login'>
-              <button className='button-sold'>Vends tes articles</button>
-            </Link>
+            <div className='button-container'>
+              {token === null ? (
+                <>
+                  <Link to='/signup'>
+                    <button className='header-button'>S'inscrire</button>
+                  </Link>
+                  <br />
+                  <Link to='/login'>
+                    <button className='header-button'>Se connecter</button>
+                  </Link>
+                </>
+              ) : (
+                <button
+                  className='header-button'
+                  onClick={() => {
+                    setUser(null);
+                    navigate('/');
+                  }}
+                >
+                  Se déconnecter
+                </button>
+              )}
+            </div>
+            <div>
+              <Link to='/login'>
+                <button className='button-sold'>Vends tes articles</button>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className='header-container'>
+          <div className='header-position'>
+            <Link to='/'>
+              <img
+                className='header-logo'
+                src={logo_vinted}
+                alt='logo_vinted'
+              />
+            </Link>
+            <div className='search-container'>
+              <div>
+                <input
+                  type='text'
+                  className='search-input'
+                  value={input}
+                  placeholder='Recherche des articles'
+                  onChange={(event) => {
+                    setInput(event.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className='button-container'>
+              {token === null ? (
+                <>
+                  <Link to='/signup'>
+                    <button className='header-button'>S'inscrire</button>
+                  </Link>
+                  <br />
+                  <Link to='/login'>
+                    <button className='header-button'>Se connecter</button>
+                  </Link>
+                </>
+              ) : (
+                <button
+                  className='header-button'
+                  onClick={() => {
+                    setUser(null);
+                    navigate('/');
+                  }}
+                >
+                  Se déconnecter
+                </button>
+              )}
+            </div>
+            <div>
+              <Link to='/login'>
+                <button className='button-sold'>Vends tes articles</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
